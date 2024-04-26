1d48ebda59db252910a1af18bc4168b3c99e7685
OsipovPawa <159521611+OsipovPawa@users.noreply.github.com>
2024-04-26 11:54:04 +0300

    kr)

---
+++ kr.cs
@@ -0,0 +1,33 @@
+﻿namespace kontr;
+abstract class Task
+{
+    private string _text;
+
+    public Task(string text)
+    {
+        _text = text;
+    }
+}
+class Task1 : Task
+{
+    public Task1(string text):base(text)
+    {
+    }
+}
+
+class Task2 : Task
+{
+    public Task2(string text):base(text)
+    {
+
+    }
+}
+class program
+{
+    static void Main(string[] args)
+    {
+        попытка не пытка
+    }
+}
+
+
