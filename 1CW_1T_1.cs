ac5f95bb67ad2b253ac14394bc64225de330481a
OsipovPawa <159521611+OsipovPawa@users.noreply.github.com>
2024-05-24 11:59:47 +0300

    КР1 вариант 1

---
+++ 1CW_1T_1.cs
@@ -0,0 +1,62 @@
+﻿using cw1;
+using System;
+
+namespace cw1
+{
+    public struct Number
+    {
+        public int Real { get; set; }
+
+        public Number(int real)
+        {
+            Real = real;
+        }
+
+        public static Number summa(Number a, Number b)
+        {
+            return new Number(a.Real + b.Real);
+        }
+
+        public static Number raznost(Number a, Number b)
+        {
+            return new Number(a.Real - b.Real);
+        }
+
+        public static Number proizvedenie(Number a, Number b)
+        {
+            return new Number(a.Real * b.Real);
+        }
+
+        public static Number chastnoe(Number a, Number b)
+        {
+            return new Number(a.Real / b.Real);
+        }
+
+        public override string ToString()
+        {
+            return $"chislo = {Real}";
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Number a = new Number(6);
+        Number b = new Number(3);
+        Number sum = Number.summa(a,b);
+        Number diff = Number.raznost(a,b);
+        Number multiple = Number.proizvedenie(a,b);
+        Number del = Number.chastnoe(a,b);
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(sum + " / summa");
+        Console.WriteLine(diff + " / Bbi4eTaHuE");
+        Console.WriteLine(multiple + " / umnojenie");
+        Console.WriteLine(del + " / delenie");
+    }
+}
+
+
---
+++ 1CW_2T_1.cs
@@ -0,0 +1,72 @@
+﻿using cw2;
+using System;
+
+namespace cw2
+{
+    public class Number
+    {
+        public int Real { get; set; }
+
+        public Number(int real)
+        {
+            Real = real;
+        }
+    }
+    public class ComplexNumber : Number
+    {
+        public int Mnimoe { get; set; }
+        public Mnimoe(int mnimoe, int real) : base(real)
+        {
+            Mnimoe = mnimoe;
+        }
+        public static ComplexNumber summa(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.Real + b.Real, a.Mnimoe + b.Mnimoe);
+        }
+
+        public static ComplexNumber raznost(Number a, Number b)
+        {
+            return new ComplexNumber(a.Real - b.Real, a.Mnimoe - b.Mnimoe);
+        }
+
+        public static ComplexNumber proizvedenie(Number a, Number b)
+        {
+            return new ComplexNumber(a.Real * b.Real, a.Mnimoe * b.Mnimoe);
+        }
+
+        public static ComplexNumber chastnoe(Number a, Number b)
+        {
+            return new ComplexNumber(a.Real / b.Real, a.Mnimoe / b.Mnumoe);
+        }
+
+        public override string ToString()
+        {
+            return $"chislo = {Real} + {Mnimoe}i";
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ComplexNumber a = new ComplexNumber(6, 2);
+            ComplexNumber b = new ComplexNumber(3, 4);
+            ComplexNumber sum = ComplexNumber.summa(a, b);
+            ComplexNumber diff = ComplexNumber.raznost(a, b);
+            ComplexNumber multiple = ComplexNumber.proizvedenie(a, b);
+            ComplexNumber del = ComplexNumber.chastnoe(a, b);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(sum + " / summa");
+            Console.WriteLine(diff + " / Bbi4eTaHuE");
+            Console.WriteLine(multiple + " / umnojenie");
+            Console.WriteLine(del + " / delenie");
+        }
+    }
+}
+
+
+
+
+
